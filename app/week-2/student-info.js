2b7ed4730274037917d474b30e57c84511cb93cc
MaanArsh <131817163+MaanArsh@users.noreply.github.com>
2024-01-22 14:58:23 -0700

    Add files via upload

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentName () {
+    return (
+        <div>
+            <p>Arshpreet Singh</p>
+            <Link href="https://github.com/MaanArsh/cprg306-assignments.git">My GitHub Repository Link</Link>
+        </div>
+    );
+};
